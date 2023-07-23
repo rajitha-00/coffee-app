5560b305d404f2d664a71752ff1eb803733bacd5
rajitha-00 <rajithatw07102@gmail.com>
2023-07-24 01:45:57 +0530

    home done

--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -1,11 +1,18 @@
-import { View, Text, Image, TouchableOpacity } from 'react-native'
-import React from 'react'
+import { View, Text, Image, TouchableOpacity, FlatList, Dimensions } from 'react-native'
 import { StatusBar } from 'expo-status-bar'
 import { SafeAreaView } from 'react-native'
 import { TextInput } from 'react-native'
+import { categories, coffeeItems } from '../constants';
+import React, { useState } from 'react'
+import {themeColors} from '../theme';
+import Carousel from 'react-native-snap-carousel';
+import CoffeeCard from '../components/coffeeCard';
 
 
+const {width, height} = Dimensions.get('window');
+const ios = Platform.OS == 'ios';
 export default function HomeScreen() {
+  const [activeCategory, setActiveCategory] = useState(1);
   return (
     <View className="flex-1 relative bg-white">
       <StatusBar />
@@ -35,10 +42,12 @@ export default function HomeScreen() {
         </View>
 
         {/* search  bar */}
-        <View className="mx-5 mt-14">
+        <View className="mx-5 shadow" style={{marginTop: height*0.06}}>
           <View className="flex-row justify-center items-center rounded-full p-1 bg-slate-300 shadow-slate-900">
             <TextInput placeholder='Search' className="p-3 flex-1 font-semibold text-gray-700" />
-            <TouchableOpacity className="p-2 rounded-full bg-amber-500">
+            <TouchableOpacity className="p-2 rounded-full"
+            style={{backgroundColor: themeColors.bgLight}}
+            >
               <Image 
                 source={require('../assets/icons/search.png')} 
                 className="w-9 h-9 rounded-full"
@@ -46,7 +55,50 @@ export default function HomeScreen() {
             </TouchableOpacity>
           </View>
         </View>
+
+        {/* categories */}
+        <View className="px-5 mt-6">
+
+          <FlatList 
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          data={categories}
+          keyExtractor={item=> item.id}
+          className="overflow-visible"
+          renderItem={({item})=>{
+            isActive = item.id==activeCategory;
+            let activeTextClass = isActive? 'text-white': 'text-gray-700';
+            return (
+              <TouchableOpacity 
+              onPress={()=> setActiveCategory(item.id)}
+              style={{backgroundColor: isActive? themeColors.bgLight: 'rgba(0,0,0,0.07)'}} 
+              className="p-4 px-5 h- mr-2 rounded-full shadow">
+                <Text className={"font-semibold " + activeTextClass}>{item.title}</Text>
+              </TouchableOpacity>
+            )
+          }}
+          />
+        </View>
+{/* coffee cards */}
+        <View className={`overflow-visible flex justify-center flex-1 ${ios? 'mt-10':''}`}>
+          <View>
+            <Carousel
+              containerCustomStyle={{overflow: 'visible'}}
+              data={coffeeItems}
+              renderItem={({item})=> <CoffeeCard item={item} />}
+              firstItem={1}
+              loop={true}
+              inactiveSlideScale={0.75}
+              inactiveSlideOpacity={0.75}
+              sliderWidth={width}
+              itemWidth={width*0.63}
+              slideStyle={{display: 'flex', alignItems: 'center'}}
+            />
+          </View>
+          
+        </View>
       </SafeAreaView>
+      
     </View>
   )
 }
