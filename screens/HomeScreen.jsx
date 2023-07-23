c0ba5eb5f8672378c4269c7cb6ed9a936b59fcf9
rajitha-00 <rajithatw07102@gmail.com>
2023-07-24 00:52:45 +0530

    search done

--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -1,7 +1,8 @@
-import { View, Text, Image } from 'react-native'
+import { View, Text, Image, TouchableOpacity } from 'react-native'
 import React from 'react'
 import { StatusBar } from 'expo-status-bar'
 import { SafeAreaView } from 'react-native'
+import { TextInput } from 'react-native'
 
 
 export default function HomeScreen() {
@@ -31,6 +32,19 @@ export default function HomeScreen() {
             source={require('../assets/icons/bell.png')} 
             className="w-9 h-9 rounded-full"
           />
+        </View>
+
+        {/* search  bar */}
+        <View className="mx-5 mt-14">
+          <View className="flex-row justify-center items-center rounded-full p-1 bg-slate-300 shadow-slate-900">
+            <TextInput placeholder='Search' className="p-3 flex-1 font-semibold text-gray-700" />
+            <TouchableOpacity className="p-2 rounded-full bg-amber-500">
+              <Image 
+                source={require('../assets/icons/search.png')} 
+                className="w-9 h-9 rounded-full"
+              />
+            </TouchableOpacity>
+          </View>
         </View>
       </SafeAreaView>
     </View>
